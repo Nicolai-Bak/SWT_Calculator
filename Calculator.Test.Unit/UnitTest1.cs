6ce1400c8a063c4850808fcddb2f5a26aed45cfa
Nicolai-Bak <nicolai.bak@gmail.com>
2022-02-03 13:16:35 +0100

    Add project files.

---
+++ Calculator.Test.Unit/UnitTest1.cs
@@ -0,0 +1,162 @@
+using NUnit.Framework;
+using System;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator();
+        }
+
+        [Test]
+        public void AddTwoPositive()
+        {
+            double a = 4;
+            double b = 5;
+            Assert.AreEqual(uut.Add(a, b), a + b);
+        }
+
+        [Test]
+        public void AddTwoNegative()
+        {
+            double a = -4;
+            double b = -5;
+            Assert.AreEqual(uut.Add(a, b), a + b);
+        }
+
+        [Test]
+        public void AddOnePositiveOneNegative()
+        {
+            double a = 4;
+            double b = -5;
+            Assert.AreEqual(uut.Add(a, b), a + b);
+        }
+
+        [Test]
+        public void SubtractTwoPositive()
+        {
+            double a = 4;
+            double b = 5;
+            Assert.AreEqual(uut.Subtract(a, b), a - b);
+        }
+
+        [Test]
+        public void SubtractTwoNegative()
+        {
+            double a = -4;
+            double b = -5;
+            Assert.AreEqual(uut.Subtract(a, b), a - b);
+        }
+
+        [Test]
+        public void SubtractOnePositiveOneNegative()
+        {
+            double a = 4;
+            double b = -5;
+            Assert.AreEqual(uut.Subtract(a, b), a - b);
+        }
+
+        [Test]
+        public void SubtractOneNegativeOnePositive()
+        {
+            double a = -4;
+            double b = 5;
+            Assert.AreEqual(uut.Subtract(a, b), a - b);
+        }
+
+
+        [Test]
+        public void MultiplyTwoPositive()
+        {
+            double a = 4;
+            double b = 5;
+            Assert.AreEqual(uut.Multiply(a, b), a * b);
+        }
+
+        [Test]
+        public void MultiplyTwoNegative()
+        {
+            double a = -4;
+            double b = -5;
+            Assert.AreEqual(uut.Multiply(a, b), a * b);
+        }
+
+        [Test]
+        public void MultiplyOnePositiveOneNegative()
+        {
+            double a = 4;
+            double b = -5;
+            Assert.AreEqual(uut.Multiply(a, b), a * b);
+        }
+
+        [Test]
+        public void MultiplyOneNegativeOnePositive()
+        {
+            double a = -4;
+            double b = 5;
+            Assert.AreEqual(uut.Multiply(a, b), a * b);
+        }
+
+        [Test]
+        public void MultiplyOnePositiveOneZero()
+        {
+            double a = 4;
+            double b =0;
+            Assert.AreEqual(uut.Multiply(a, b), a * b);
+        }
+
+        [Test]
+        public void PowerTwoPositive()
+        {
+            double a = 4;
+            double b = 5;
+            Assert.AreEqual(uut.Power(a, b), Math.Pow(a, b));
+        }
+
+        [Test]
+        public void PowerTwoNegative()
+        {
+            double a = -4;
+            double b = -5;
+            Assert.AreEqual(uut.Power(a, b), Math.Pow(a, b));
+        }
+
+        [Test]
+        public void PowerOnePositiveOneNegative()
+        {
+            double a = 4;
+            double b = -5;
+            Assert.AreEqual(uut.Power(a, b), Math.Pow(a, b));
+        }
+
+        [Test]
+        public void PowerOneNegativeOnePositive()
+        {
+            double a = -4;
+            double b = 5;
+            Assert.AreEqual(uut.Power(a, b), Math.Pow(a, b));
+        }
+
+        [Test]
+        public void PowerOnePositiveOneZero()
+        {
+            double a = 4;
+            double b = 0;
+            Assert.AreEqual(uut.Power(a, b), Math.Pow(a, b));
+        }
+
+        [Test]
+        public void PowerOneZeroOnePositive()
+        {
+            double a = 0;
+            double b = 5;
+            Assert.AreEqual(uut.Power(a, b), Math.Pow(a,b));
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,216 @@
+﻿
+Calculator.Calculator c = new Calculator.Calculator();
+double a, b;
+
+Console.WriteLine("Test Add");
+Console.Write("\ttwo positive numbers\t");
+a = 4;
+b = 5;
+Console.WriteLine(c.Add(a, b));
+Console.Write("\ttwo negative numbers\t");
+a = -4;
+b = -5;
+Console.WriteLine(c.Add(a, b));
+
+Console.Write("\tOne positve, one negative\t");
+a = 4;
+b = -5;
+Console.WriteLine(c.Add(a, b));
+
+Console.WriteLine("Test Subtract");
+Console.Write("\ttwo positive numbers \t");
+a = 4;
+b = 5;
+Console.WriteLine(c.Subtract(a, b));
+
+
+Console.Write("\ttwo negative numbers\t");
+a = -4;
+b = -5;
+Console.WriteLine(c.Subtract(a, b));
+
+Console.Write("\tOne positve, one negative\t");
+a = 4;
+b = -5;
+Console.WriteLine(c.Subtract(a, b));
+
+Console.Write("\tOne negative, one positive\t");
+a = -4;
+b = 5;
+Console.WriteLine(c.Subtract(a, b));
+
+Console.WriteLine("Test Multiply");
+Console.Write("\ttwo positive numbers\t");
+a = 4;
+b = 5;
+Console.WriteLine(c.Multiply(a, b));
+
+Console.Write("\ttwo negative numbers\t");
+a = -4;
+b = -5;
+Console.WriteLine(c.Multiply(a, b));
+
+Console.Write("\tOne positve, one negative\t");
+a = 4;
+b = -5;
+Console.WriteLine(c.Multiply(a, b));
+
+Console.Write("One negative, one positive");
+a = -4;
+b = 5;
+Console.WriteLine(c.Multiply(a, b));
+
+Console.Write("Zero");
+a = 4;
+b = 0;
+Console.WriteLine(c.Multiply(a, b));
+
+Console.WriteLine("Test Power");
+Console.Write("\ttwo positive numbers\t");
+a = 4;
+b = 5;
+Console.WriteLine(c.Power(a, b));
+
+
+//      two negative numbers
+a = -4;
+b = -5;
+Console.WriteLine(c.Power(a, b));
+
+//      One positve, one negative
+a = 4;
+b = -5;
+Console.WriteLine(c.Power(a, b));
+
+//      One negative, one positive
+a = -4;
+b = 5;
+Console.WriteLine(c.Power(a, b));
+
+//      Zero x
+a = 0;
+b = 5;
+Console.WriteLine(c.Power(a, b));
+
+//      Zero exp
+a = 4;
+b = 0;
+Console.WriteLine(c.Power(a, b));
+
+
+
+//using System;
+
+//namespace Calculator
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Calculator c = new Calculator();
+//            double a, b;
+
+//            Console.WriteLine("Test Add()");
+//            Console.WriteLine("two positive numbers");
+//            a = 4; 
+//            b = 5;
+//            Console.WriteLine(c.Add(a, b));
+//            Console.WriteLine("two negative numbers");
+//            a = -4;
+//            b = -5;
+//            Console.WriteLine(c.Add(a, b));
+
+//            Console.WriteLine("One positve, one negative");
+//            a = 4;
+//            b = -5;
+//            Console.WriteLine(c.Add(a, b));
+
+//            //      Test Subtract
+//            //      two positive numbers
+//            a = 4;
+//            b = 5;
+//            Console.WriteLine(c.Subtract(a, b));
+
+
+//            //      two negative numbers
+//            a = -4;
+//            b = -5;
+//            Console.WriteLine(c.Subtract(a, b));
+
+//            //      One positve, one negative
+//            a = 4;
+//            b = -5;
+//            Console.WriteLine(c.Subtract(a, b));
+
+//            //      One negative, one positive
+//            a = -4;
+//            b = 5;
+//            Console.WriteLine(c.Subtract(a, b));
+
+
+//            //      Test Multiply
+//            //      two positive numbers
+//            a = 4;
+//            b = 5;
+//            Console.WriteLine(c.Multiply(a, b));
+
+
+//            //      two negative numbers
+//            a = -4;
+//            b = -5;
+//            Console.WriteLine(c.Multiply(a, b));
+
+//            //      One positve, one negative
+//            a = 4;
+//            b = -5;
+//            Console.WriteLine(c.Multiply(a, b));
+
+//            //      One negative, one positive
+//            a = -4;
+//            b = 5;
+//            Console.WriteLine(c.Multiply(a, b));
+
+//            //      Zero
+//            a = 4;
+//            b = 0;
+//            Console.WriteLine(c.Multiply(a, b));
+
+//            //      Test Power
+//            //      two positive numbers
+//            a = 4;
+//            b = 5;
+//            Console.WriteLine(c.Power(a, b));
+
+
+//            //      two negative numbers
+//            a = -4;
+//            b = -5;
+//            Console.WriteLine(c.Power(a, b));
+
+//            //      One positve, one negative
+//            a = 4;
+//            b = -5;
+//            Console.WriteLine(c.Power(a, b));
+
+//            //      One negative, one positive
+//            a = -4;
+//            b = 5;
+//            Console.WriteLine(c.Power(a, b));
+
+//            //      Zero x
+//            a = 0;
+//            b = 5;
+//            Console.WriteLine(c.Power(a, b));
+
+//            //      Zero exp
+//            a = 4;
+//            b = 0;
+//            Console.WriteLine(c.Power(a, b));
+
+
+//        }
+
+
+
+//    }
+//}
